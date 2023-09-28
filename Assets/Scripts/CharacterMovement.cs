665d7b74d8d62519badf191bc6f0328af007fd63
Jia Xuan Li <2154201@edu.vaniercollege.qc.ca>
2023-09-28 10:04:31 -0400

    Y camera rotation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -15,7 +15,7 @@ public class CharacterMovement : MonoBehaviour
     public bool groundedPlayer;
 
     public float mouseSensitivyX = 5.0f;
-    public float mouseSensitivyY = 0.2f;
+    public float mouseSensitivyY = 2.5f;
 
     private Vector3 move;
     private float jumpHeight = 1f;
@@ -57,6 +57,19 @@ public class CharacterMovement : MonoBehaviour
     void UpdateRotation()
     {
         transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensitivyX, 0, Space.Self);
+
+        // rotate the follow part
+        float rotationX = followPart.transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * 1.5f;
+        if (rotationX > 45 && rotationX < 90)
+        {
+            rotationX = 45;
+        } else if (rotationX < 325 && rotationX >= 90)
+        {
+            rotationX = 325;
+        }
+
+        followPart.transform.localEulerAngles = new Vector3(rotationX, 0, 0);
+
     }
 
     void UpdateAnimator()
